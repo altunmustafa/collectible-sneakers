f5b83bc2f818f7d26bf9e550163a0e00f6343918
Mustafa Altun <altunmstf@gmail.com>
2024-05-26 00:19:39 +0300

    fix: content-box items-center (max-md)

--- src/components/content-box.tsx
+++ src/components/content-box.tsx
@@ -67,7 +67,7 @@ const ContentBox: React.FC<IContentBoxProps> = ({ density = "normal", image, ico
   }
 
   return (
-    <div className={`flex flex-col overflow-hidden w-full h-fit rounded-[10px] text-center md:text-left ${shadow ? tailwindShadow : ""} ${border ? "border border-slate-900" : ""} ${className}`}>
+    <div className={`flex flex-col max-md:items-center overflow-hidden w-full h-fit rounded-[10px] text-center md:text-left ${shadow ? tailwindShadow : ""} ${border ? "border border-slate-900" : ""} ${className}`}>
       {/* Image */}
       {image}
       {/* Content */}
